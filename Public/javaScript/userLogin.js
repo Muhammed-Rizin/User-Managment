e548d37a1fe70daf9aaed40d4c1f5da30e8261a6
MuhammeRizin <123790489+MuhammeRizin@users.noreply.github.com>
2023-03-16 09:04:25 +0530

    password check

--- Public/javaScript/userLogin.js
+++ Public/javaScript/userLogin.js
@@ -3,11 +3,15 @@ function show(){
     let para1 = document.getElementById('para1')
     if(email==""){
         para1.innerText = 'Enter Email'
+    }else{
+        para1.innerText = null
     }
 
     let password = document.getElementById("password").value
     let para2 = document.getElementById("para2")
     if(password==""){
         para2.innerText = "Enter Password"
+    }else{
+        para2.innerText = null
     }
 }
--- Public/javaScript/userRegister.js
+++ Public/javaScript/userRegister.js
@@ -3,17 +3,23 @@ function show(){
     let para1 = document.getElementById('para1')
     if(name==""){
         para1.innerText = 'EnterName'
+    }else{
+        para1.innerText = null
     }
 
     let email = document.getElementById('email').value
     let para2 = document.getElementById('para2')
     if(email==""){
         para2.innerText = 'Enter Email'
+    }else{
+        para2.innerText = null
     }
 
     let password = document.getElementById("pass").value
     let para3 = document.getElementById("para3")
     if(password==""){
         para3.innerText = "Enter Password"
+    }else{
+        para3.innerText = null
     }
 }   
